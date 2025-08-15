57bd86e5b3888d75540f7d34c388e68c2c0bfd62
fogsaturate <57604850+fogsaturate@users.noreply.github.com>
2025-08-15 13:09:56 -0400

    Update BoardConfig.h

--- configs/WiiGuitar/BoardConfig.h
+++ configs/WiiGuitar/BoardConfig.h
@@ -1,8 +1,3 @@
-/*
- * SPDX-License-Identifier: MIT
- * SPDX-FileCopyrightText: Copyright (c) 2024 OpenStickCommunity (gp2040-ce.info)
- */
-
 #ifndef PICO_BOARD_CONFIG_H_
 #define PICO_BOARD_CONFIG_H_
 
